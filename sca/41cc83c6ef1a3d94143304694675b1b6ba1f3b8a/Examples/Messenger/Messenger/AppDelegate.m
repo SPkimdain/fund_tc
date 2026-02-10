b09fa7693f54f784443ad8f2775c96054f4455e7
SPkimdain <dain@fasoo.com>
2026-02-10 16:54:07 +0900

    cocoapods 사이즈 큰 것 변경

---
+++ sca/41cc83c6ef1a3d94143304694675b1b6ba1f3b8a/Examples/Messenger/Messenger/AppDelegate.m
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.m
+//  Messenger
+//
+//  Created by Ignacio Romero Zurbuchen on 8/15/14.
+//  Copyright (c) 2014 Slack Technologies, Inc. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+#import "SLKTextViewController.h"
+#import "MessageViewController.h"
+
+@interface AppDelegate () <UIPopoverControllerDelegate>
+@end
+
+@implementation AppDelegate
+            
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[MessageViewController new]];
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
+
+@end
